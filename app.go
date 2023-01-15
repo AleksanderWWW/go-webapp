f74041405748fbb7eddf7fa724affed6440fefcc
AleksanderWWW <alwojnarowicz@gmail.com>
2023-01-15 17:18:34 +0100

    add function to insert posted product data to mongo collection

--- app.go
+++ app.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"io/ioutil"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 
 	"github.com/AleksanderWWW/go-webapp/backend"
+	"github.com/AleksanderWWW/go-webapp/resources"
 	"github.com/AleksanderWWW/go-webapp/utils"
 )
 
@@ -30,14 +33,31 @@ func retrieveSingle(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(p)
 }
 
+func insertSingle(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var p resources.Product
+	json.Unmarshal(reqBody, &p)
+	
+	coll := backend.ConnectToMongo(utils.DatabaseName, utils.CollectionName)
+
+	defer backend.CloseMongoConnection(coll.Database().Client())
+
+	backend.InsertProduct(coll, p)
+}
+
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", indexHandler)
 	myRouter.HandleFunc("/all", retrieveAll)
 	myRouter.HandleFunc("/product/{id}", retrieveSingle)
+	myRouter.HandleFunc("/product", insertSingle).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+	
 	handleRequests()
 }
